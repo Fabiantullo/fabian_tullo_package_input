a40472cfe33ec4399e16d85ab0c08a5e6bd34418
Fabiantullo <fabiantullorancati@gmail.com>
2024-09-11 00:01:34 -0300

    Segundo commit finalizando el programa

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from Package_Input.input import *
-print(get_int("Dame un numero: ", "Hay un error", 1, 100, 5))
+
+print(get_int(1, "Hay un error", 1, 100, 5))
+
 print(get_float("Dame un flotante: ", "Hay un error", 5, 10, 5))
+
 print(get_string("Dame unos caracteres:", 3))
